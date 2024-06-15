8ddc3b8ae9729ba572dd03296e3fe81f08b108d4
Aliev Madjit <madzitaliev51@gmail.com>
2024-06-15 17:03:34 +0600

    Создал компонент формы, состояние и обработчик изменений

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
+import { Container, Grid } from "@mui/material";
+import ChatForm from "./Components/ChatForm";
 
 function App() {
-
   return (
-    <>
-    </>
-  )
+    <Container maxWidth='md'>
+      <Grid container direction="column">
+        <Grid item xs={9}>
+          Сообщения
+        </Grid>
+        <Grid item xs={3}>
+          <ChatForm/>
+        </Grid>
+      </Grid>
+    </Container>
+  );
 }
 
-export default App
+export default App;
---
+++ src/Components/ChatForm.tsx
@@ -0,0 +1,60 @@
+import { Box, Button, Grid, TextField } from "@mui/material";
+import { useState } from "react";
+import { Message } from "../types";
+
+const initialState: Message = {
+  author: "",
+  message: "",
+  datetime: "",
+};
+
+const ChatForm = () => {
+  const [message, setMessage] = useState<Message>(initialState);
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+
+    setMessage((prevState) => {
+      return { ...prevState, [name]: value };
+    });
+  };
+
+  const onSubmit = (e:React.FormEvent) => {
+    e.preventDefault();
+    console.log(message);
+  };
+
+  return (
+    <Box component="form" onSubmit={onSubmit}>
+      <Grid container spacing={2} direction="column">
+        <Grid item xs>
+          <TextField
+            value={message.message}
+            fullWidth
+            multiline
+            rows={3}
+            id="message"
+            label="Введите сообщение"
+            name="message"
+            onChange={onChange}
+          />
+        </Grid>
+        <Grid item xs>
+          <TextField
+            value={message.author}
+            fullWidth
+            id="author"
+            label="Введите имя"
+            name="author"
+            onChange={onChange}
+          />
+        </Grid>
+        <Grid item xs>
+          <Button variant="contained" type="submit">Отправить</Button>
+        </Grid>
+      </Grid>
+    </Box>
+  );
+};
+
+export default ChatForm;
